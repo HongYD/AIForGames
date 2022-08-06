943677ab40b5e32155a86b7b8c4c1a31e0f9bdeb
Dennis <nirvanagun93@gmail.com>
2022-08-06 22:51:14 +0800

    update Predicting physics

--- SteeringBehavior/Assets/Scripts/PredictingPhysics/Cannon.cs
+++ SteeringBehavior/Assets/Scripts/PredictingPhysics/Cannon.cs
@@ -16,32 +16,46 @@ public class Cannon : MonoBehaviour
     public GameObject bullet;
     public float muzzleV;
     public Vector3 gravity;
+    public int bulletTrajectoryLen;
     private CannonState _state;
     private GameObject _cannon;
     private GameObject _muzzle;
     private GameObject _tube;
+    private GameObject _tail;
     private Vector3 _curFireDir;
-    private float _angleDiffLimit = 0.1f;
+    private Vector3 _culcDir;
+    private Vector3 _preCulcDir;
+    private List<Vector3> _bulleyTrajectory;
+    private float frameRate = 1.0f / 30.0f;
+    [SerializeField]
+    private float _time;
+    
     // Start is called before the first frame update
     void Start()
     {
         _state = CannonState.None;
         _cannon = this.gameObject;
         _muzzle = _cannon.transform.GetChild(0).GetChild(1).GetChild(0).gameObject;
+        _tail= _cannon.transform.GetChild(0).GetChild(1).GetChild(1).gameObject;
         _tube = _cannon.transform.GetChild(0).GetChild(1).gameObject;
-        _curFireDir = _curFireDir = (_muzzle.transform.position - _tube.transform.position).normalized;
+        _curFireDir = (_muzzle.transform.position - _tail.transform.position).normalized;
         _tube.transform.rotation = Quaternion.LookRotation(_curFireDir);
+        _culcDir = Vector3.zero;
+        _preCulcDir = Vector3.zero;
+        _bulleyTrajectory = new List<Vector3>();
+        _time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         FixCannon();
+        CalculateBulletTrajectory();
     }
 
-    Vector3 CalculateFiringSolution(Vector3 start, Vector3 end)
+    Vector3 CalculateFiringSolution(Vector3 start, Vector3 end, out float ttt)
     {
-        float ttt;
+        ttt = 0;
         Vector3 delta = end - start;
         float a = Vector3.Dot(gravity, gravity);
         float b = -4 * (muzzleV * muzzleV - Vector3.Dot(gravity, delta));
@@ -89,8 +103,57 @@ public class Cannon : MonoBehaviour
 
     void FixCannon()
     {
-        Vector3 newDir = CalculateFiringSolution(_muzzle.transform.position, target.transform.position);
-        Debug.DrawRay(_muzzle.transform.position, newDir, Color.red);
-        _tube.transform.rotation = Quaternion.LookRotation(newDir);
+        _culcDir = CalculateFiringSolution(_muzzle.transform.position, target.transform.position, out _time);
+        _curFireDir = (_muzzle.transform.position - _tail.transform.position).normalized;
+        Quaternion newRot = Quaternion.identity;
+        if (_culcDir != Vector3.zero)
+        {
+            newRot = Quaternion.LookRotation(_culcDir);
+        }
+        else
+        {
+            newRot = Quaternion.LookRotation(_preCulcDir);
+        }
+        float rotInX = newRot.eulerAngles.x;
+        float rotInY = newRot.eulerAngles.y;
+        float rotInZ = newRot.eulerAngles.z;
+        Vector2 targetXZ = new Vector2(target.transform.position.x, target.transform.position.z);
+        Vector2 cannonXZ = new Vector2(this.transform.position.x, this.transform.position.z);
+        if (Vector2.Distance(targetXZ, cannonXZ) > 4.0f && _culcDir != Vector3.zero)
+        {
+            _tube.transform.rotation = Quaternion.Euler(rotInX, rotInY, rotInZ);
+            _preCulcDir = _culcDir;
+        }
+        Debug.DrawRay(_muzzle.transform.position, _culcDir, Color.red);
+        Debug.DrawRay(_muzzle.transform.position, _curFireDir, Color.blue);
+    }
+
+    void CalculateBulletTrajectory()
+    {
+        if (_culcDir != Vector3.zero)
+        {
+            _bulleyTrajectory.Clear();
+        }
+        Vector3 p0 = _muzzle.transform.position;
+        float t = 0;
+        for (int i = 0; i < bulletTrajectoryLen && t <_time; i++)
+        {
+            t += _time * frameRate;
+            Vector3 pt = p0 + _culcDir * muzzleV * t + (gravity * t * t) / 2;
+            _bulleyTrajectory.Add(pt);
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_bulleyTrajectory != null && _bulleyTrajectory.Count > 0)
+        {
+            for (int i = 0; i < _bulleyTrajectory.Count; i++)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawSphere(_bulleyTrajectory[i], 0.1f);
+            }
+        }
     }
 }
